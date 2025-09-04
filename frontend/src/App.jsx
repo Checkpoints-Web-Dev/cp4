606960460df9e5cecbccfe2210b33cfb0ec17bb3
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2025-09-04 20:42:37 -0300

    adicionando componente Lojas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Carros from "./components/Carros";
 import Header from "./components/Header";
+import Lojas from "./components/Lojas";
 
 function App() {
   return (
     <>
       <Header />
       <Carros />
+      <Lojas />
     </>
   );
 }
