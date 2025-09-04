e412a5b68e8ebeb952bdd8cc7c03994c073bf0c3
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2025-09-04 19:47:48 -0300

    adicionando as rotas da aplicacao

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,22 @@ const port = 5001;
 app.use(cors());
 app.use(express.json());
 
+app.get("/carros", (req, res) => {
+  res.send("Lista de carros disponíveis");
+});
+
+app.get("/lojas", (req, res) => {
+  res.send("Bem-vindo à loja de carros!");
+});
+
+app.get("/sobre", (req, res) => {
+  res.send("Esta é a página sobre nós.");
+});
+
+app.get("/contato", (req, res) => {
+  res.send("Página de contato");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
