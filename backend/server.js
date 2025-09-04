af5fc64187c21ae76e3fb64f16970dfa4f0f7895
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2025-09-04 19:37:54 -0300

    criando backend inicial com o servidor

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const port = 5001;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
